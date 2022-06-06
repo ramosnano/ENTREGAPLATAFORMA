f0ae3a0a8cfa2c30fe1c6b1c64b2184698b1ded9
Fer Ramos <arq.ramosfernando@gmail.com>
2022-06-06 18:57:22 -0300

    2.1

--- plataforma/urls.py
+++ plataforma/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from plataforma.views import index
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', index, name = 'index'),
@@ -10,3 +13,4 @@ urlpatterns = [
     path('documentales/', include('documental.urls')),
 
 ]
+
