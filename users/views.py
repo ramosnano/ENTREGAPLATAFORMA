2814f11c0219e36c394c1a1e622dae836c2ce5e7
Gaston Fernandez <gaston.fernandez.3101@gmail.com>
2022-07-05 10:04:01 -0300

    loginperfiles

--- users/views.py
+++ users/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
 from users.models import User_profile
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-
-class List_profiles(ListView):
+class List_profiles(LoginRequiredMixin, ListView):
     model = User_profile
     template_name= 'profiles.html'
     queryset = User_profile.objects.all()
